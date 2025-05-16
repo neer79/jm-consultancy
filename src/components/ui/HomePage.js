76a9a0d2af217a0b370ceb61a905ca6f87ce5177
Neeraj <neer79@live.com>
2025-05-16 19:37:50 +0530

    changes

--- src/components/ui/HomePage.js
+++ src/components/ui/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
+import { Card, CardContent } from "./src/components/ui/card";
+import { Button } from "./src/components/ui/button";
 
 export default function HomePage() {
   return (
