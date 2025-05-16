8ed7f710d4dc86b33c4adede924b00fa800cfd6b
Neeraj <neer79@live.com>
2025-05-16 19:49:30 +0530

    commit all and push

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import React from 'react';
-import HomePage from '../components/ui/HomePage';
-
-export default function Page() {
-  return <HomePage />;
-}
--- src/components/ui/button.js
+++
@@ -1,2 +0,0 @@
-import { Button } from "./components/ui/button";
-import { Card, CardContent } from "./components/ui/card";
